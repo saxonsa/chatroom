17865866636f24e79c352ce2a2845d23c1d57a2b
Poalry <43143420+Mano-Liaoyan@users.noreply.github.com>
2020-12-02 21:55:46 +0800

    Finish history insertion and selection into database

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -11,7 +11,6 @@
 #pragma comment(lib,"ws2_32.lib")
 #pragma comment(lib,"libmysql.lib")
 
-
 /*
 	exit_clean: (safe exit function)
 		description: Signal callback function, when unknown exit Ctrl-c, will clear the process
@@ -22,6 +21,75 @@
 void exit_clean(int arg) {
 	WSACleanup();
 	exit(0);
+}
+
+void insert_into_database(char content[]){
+	char toInsertHistory[250] = "Insert INTO history(user_name,content) VALUES(";
+	char userName[] = "'David','";
+
+	char finalString[300];
+	
+	// Concat strings
+	strcat(toInsertHistory,userName);
+	strcat(toInsertHistory,content);
+	strcat(toInsertHistory,"');");
+
+	strcpy(finalString,toInsertHistory);	
+
+	ret = mysql_query(&mysqlConnect, finalString); // Pass the query to database
+
+	// If the query failed, close the function
+	if (ret != 0) {
+		printf("Query failed...Error: %s\n", mysql_error(&mysqlConnect));
+		// return;
+	}
+
+	// A hint that shows the insertion is complte
+	// printf("History record succeed! \n");
+
+	return;
+}
+
+void search_history(){
+	// The selection query
+	ret = mysql_query(&mysqlConnect, "SELECT * FROM `history`");
+
+	// If the query failed, close the function
+	if (ret != 0) {
+		printf("Query failed...Error: %s\n", mysql_error(&mysqlConnect));
+		return;
+	}
+
+	res = mysql_store_result(&mysqlConnect);// Check the res value
+
+	if (res) {
+		int fieldCount = mysql_field_count(&mysqlConnect);
+		//Print the result table
+		if (fieldCount > 0) {
+			int column = mysql_num_fields(res);
+			int row = mysql_num_rows(res);
+			for (int i = 0; field = mysql_fetch_field(res); i++) { 
+				printf("%25s", field->name);
+				printf(" |");
+			}
+			printf("\n");
+			while (nextRow = mysql_fetch_row(res)) {
+				for (int j = 0; j < column; j++) {
+					printf("%25s", nextRow[j]);
+					printf(" |");
+				}
+				printf("\n");
+			}
+		}
+		else {
+			printf("No resullt. This is the result of a character splitting query... \n");
+		}
+	}
+	else {
+		printf("mysql_store_result...Error: %s\n", mysql_error(&mysqlConnect));
+	}
+
+	return;
 }
 
 // function to accept a connection
@@ -52,6 +120,9 @@ void accept_conn(void *dummy) {
 
 			printf("Bytes Received: %d, message: %s from %s\n", msg_len, szBuff, inet_ntoa(client_addr.sin_addr));
 
+			insert_into_database(szBuff); // Insert the history into the database
+			search_history();// Search history from database
+
 			/* Send message back to Client */
 			msg_len = send(sub_sock, szBuff, sizeof(szBuff), 0);
 			if (msg_len <= 0){
@@ -68,11 +139,7 @@ void accept_conn(void *dummy) {
 
 int main(int argc, char **argv){
 
-	MYSQL mysqlConnect; 
-	MYSQL_RES *res;
-	MYSQL_FIELD *field; 
-	MYSQL_ROW nextRow;
-	int ret;
+
 
 	mysql_init(&mysqlConnect);
 	if (!(mysql_real_connect(&mysqlConnect, "localhost", "root", "", "chatroom", 3306, NULL, 0))) {
