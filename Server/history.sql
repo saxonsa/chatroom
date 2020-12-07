cde568a2d0fe155808d46ea57c24e2a20aa7bd40
Poalry <43143420+Mano-Liaoyan@users.noreply.github.com>
2020-12-07 20:06:34 +0800

    change the sql file

--- Server/history.sql
+++ Server/history.sql
@@ -1,6 +1,6 @@
 create table history(
 `hid` int primary key not null auto_increment,
 `user_name` varchar(30),
-'create_time' DATETIME DEFAULT now(),
+`create_time` DATETIME DEFAULT now(),
 `content` text(1200)
 )default charset=utf8;
